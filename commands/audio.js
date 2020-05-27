11b495674238eb175c0e611b20efcedf6ffb2bc4
trangj <joshua.trang@hotmail.com>
2020-05-26 23:35:58 -0400

    add more pics but audio links expire, so gotta change that

--- commands/audio.js
+++ commands/audio.js
@@ -2,30 +2,37 @@ module.exports = {
   name: "audio",
   description: "Play an audio clip of Robert",
   args: true,
-  usage: "<# from 1-6>",
+  usage: "<# from 0-6>",
   execute(message, args) {
-    if (!message.member.voice.channel) return;
-    message.member.voice.channel
-      .join()
-      .then((connection) => {
-        if (args[0] == "1") {
-          const dispatcher = connection.play(
-            "https://www.mboxdrive.com/last%20moments%20before%20jew%20was%20murdered%20by%20ss.mp3"
-          );
+    if (!message.member.voice.channel) {
+      message.reply("you have to be in a voice channel to use this command.");
+      return;
+    }
+
+    const audio = [
+      "",
+      "https://www.mboxdrive.com/last%20moments%20before%20jew%20was%20murdered%20by%20ss.mp3",
+    ];
+
+    if (args[0] < audio.length) {
+      message.member.voice.channel
+        .join()
+        .then((connection) => {
+          const dispatcher = connection.play(audio[args[0]]);
           dispatcher.on("start", () => {
-            console.log("audio.mp3 is now playing!");
+            console.log("audio has started");
           });
           dispatcher.on("finish", () => {
-            console.log("audio.mp3 has finished playing!");
+            console.log("audio has finished");
             connection.disconnect();
           });
           dispatcher.on("error", console.error);
-        } else {
-          message.reply("pick an argument from 1-6.");
-        }
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    } else {
+      message.reply("pick an argument from 0-6.");
+    }
   },
 };
--- commands/rob.js
+++ commands/rob.js
@@ -16,6 +16,20 @@ module.exports = {
       message.channel.send("https://imgur.com/xvhEr7e");
     } else if (args[0] == "6") {
       message.channel.send("https://imgur.com/AUzF87h");
+    } else if (args[0] == "7") {
+      message.channel.send("https://imgur.com/WNwtb7Z");
+    } else if (args[0] == "8") {
+      message.channel.send("https://imgur.com/2diRh8e");
+    } else if (args[0] == "9") {
+      message.channel.send("https://imgur.com/MvSq0a2");
+    } else if (args[0] == "10") {
+      message.channel.send("https://imgur.com/aR2NxTy");
+    } else if (args[0] == "11") {
+      message.channel.send("https://imgur.com/cP7rUoy");
+    } else if (args[0] == "12") {
+      message.channel.send("https://imgur.com/Hv6KeMS");
+    } else if (args[0] == "13") {
+      message.channel.send("https://imgur.com/gZdFr3U");
     } else {
       message.reply("pick an argument from 1-6.");
     }
